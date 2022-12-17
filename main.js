7727127268b128943f867734867317da50fe43bd
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2022-12-17 12:18:49 +0100

    Added file main.js.

---
+++ main.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+var randomWords = require('random-words');
+var request = require('request');
+
+
+var apiKey = fs.readFileSync("./info.txt",'utf8')
+
+var headers = {
+    'x-api-key': apiKey,
+    'x-product': 'MySampleApp/1.0'
+};
+
+var randomWord = randomWords();
+
+var options = {
+
+    url: "https://stock.adobe.io/Rest/Media/1/Search/Files?locale=en_US&search_parameters%5Bwords%5D=" + randomWord + "&result_columns%5B%5D=keywords&result_columns%5B%5D=id",
+    headers: headers
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        fs.writeFile("./test.txt", body + "\n\n", (err) => {
+            if (err) {
+                console.error(err);
+            return;
+              }
+        });
+
+        console.log(JSON.stringify(body, null, 10));
+        console.log(randomWord);
+    }
+}
+
+request(options, callback);
