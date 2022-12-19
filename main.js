64bdb6bfe54456a0f2d1ae3f6d181512057c7a91
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2022-12-19 16:20:23 +0100

    Writes all keywords in document. Groups are on different lines. Wrapped in function to randomize the API request every time.

--- main.js
+++ main.js
@@ -1,66 +1,66 @@
-const fs = require("fs");
-var randomWords = require('random-words');
-var request = require('request');
+function program() {
+    const fs = require("fs");
+    var randomWords = require('random-words');
+    var request = require('request');
 
 
-var apiKey = fs.readFileSync("./info.txt",'utf8')
+    var apiKey = fs.readFileSync("./info.txt",'utf8')
 
-var headers = {
-    'x-api-key': apiKey,
-    'x-product': 'MySampleApp/1.0'
-};
 
-var randomWord = randomWords();
+    var headers = {
+        'x-api-key': apiKey,
+        'x-product': 'MySampleApp/1.0'
+    };
 
-var options = {
+    var randomWord = randomWords();
+    var options = {
+        url: "https://stock.adobe.io/Rest/Media/1/Search/Files?locale=en_US&search_parameters%5Bwords%5D=" + randomWord + "&result_columns%5B%5D=keywords&result_columns%5B%5D=id",
+        headers: headers
+    };
 
-    url: "https://stock.adobe.io/Rest/Media/1/Search/Files?locale=en_US&search_parameters%5Bwords%5D=" + randomWord + "&result_columns%5B%5D=keywords&result_columns%5B%5D=id",
-    headers: headers
-};
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        /*fs.writeFile("./test.txt", body + "\n\n", (err) => {
-            if (err) {
-                console.error(err);
-            return;
-              }
-        });
-
-        console.log(JSON.stringify(body, null, 10));
-        console.log(randomWord);
-        
-
-        /*for (let y = 0; y < obj.files.length; y++) {
-            for (let i = 0; i < obj.files[y].keywords.length; i++) {
-                console.log(obj.files[y].keywords[i].name)
-            }
-            console.log("-----------\n") */
-
-        const obj = JSON.parse(body)
-
-        console.log(obj.files[0].keywords)
-
-        for (let i = 0; i < obj.files[0].keywords.length; i++) {
-            fs.appendFileSync("./test.txt", obj.files[0].keywords[i].name + " ", (err) => {
-                if (err) {
-                    console.error(err);
-                return;
-                  }
-            })
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
             
-            console.log(obj.files[0].keywords[i].name, i)
-        }
+            const obj = JSON.parse(body)
 
-        fs.appendFileSync("./test.txt", " realistic high quality detailed --v 4 --ar 3:2 ", (err) => {
-            if (err) {
-                console.error(err);
-            return;
-              }
-        })
-        
+            console.log(obj.files)
+
+            for (let y = 0; y < obj.files.length; y++) {
+                for (let i = 0; i < obj.files[y].keywords.length; i++) {
+                    fs.appendFileSync("./test.txt", obj.files[y].keywords[i].name + " ", (err) => {
+                        if (err) {
+                            console.error(err);
+                        return;
+                        }
+                    })
+                    
+                    console.log(obj.files[y].keywords[i].name, i)
+                }
+
+                fs.appendFileSync("./test.txt", " realistic high quality detailed --v 4 --ar 3:2\r\n", (err) => {
+                    if (err) {
+                        console.error(err);
+                    return;
+                    }
+                })
+            }
+        } 
     }
+
+   
+    request(options, callback);
 }
 
-request(options, callback);
 
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+//async function load () { 
+for (let i = 0; i < 100; i++) {
+    program();
+
+    //await timer(3000); 
+    }
+//}
+
+//load();
