6926dd3edab3476ea46b932ddfdce63cccf8187d
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2022-12-21 00:50:23 +0100

    Added support for varying file formats. Removed newline glitch.

--- imageconverter.py
+++ imageconverter.py
@@ -1,18 +1,21 @@
 from PIL import Image 
 import os 
 
+inputFormat = "png"
+outputFormat = "jpg"
+
 file = open('./info.txt')
 content = file.readlines()
 inputPath = content[3].replace("\n", "")
-outputPath = content[4]
+outputPath = content[4].replace("\n", "")
 print(inputPath, outputPath)
 
 
 for file in os.listdir(inputPath): 
-    if file.endswith(".png"): 
+    if file.endswith(f".{inputFormat}"): 
         img = Image.open(inputPath + "\\" + file)
         file_name, file_ext = os.path.splitext(inputPath + "\\" + file)
         print(os.path.splitext(inputPath + "\\" + file))
         print(file_name)
-        print(f'{outputPath}\{file.replace(".png", "")}.jpg')
-        img.save(f'{outputPath}\{file.replace(".png", "")}.jpg')
+        print(f'{outputPath}\{file.replace(f".{inputFormat}", "")}.{outputFormat}')
+        img.save(f'{outputPath}\{file.replace(f".{inputFormat}", "")}.{outputFormat}')
