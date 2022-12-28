9b31e14bb3e4c09e54737d21c27c5827b9332420
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2022-12-29 00:39:45 +0100

    Added ImageDescriber.py.

---
+++ ImageDescriber.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+file = open('./Data.txt')
+content = file.readlines()
+OcpKey = content[7].replace("\n", "")
+imageUrl = content[8].replace("\n", "")
+
+header = {"Ocp-Apim-Subscription-Key": OcpKey}
+data = {"url": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Dhaulagiri_mountain.jpg"}
+
+response = requests.post(imageUrl, headers=header, json=data)
+responseDict = json.loads(str(response.text))
+responseDescription = responseDict["description"]["captions"][0]["text"]
+print(responseDescription)
