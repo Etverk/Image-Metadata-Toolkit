23d5d77ed13571fa82d41a59244ffedc44b0088a
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2022-12-30 22:15:04 +0100

    Added for loop that loops through image folder.

--- ImageDescriber.py
+++ ImageDescriber.py
@@ -5,9 +5,12 @@ from msrest.authentication import CognitiveServicesCredentials
 
 from array import array
 import os
-from PIL import Image
 import sys
 import time
+
+from exif import Image as ExifImage
+from PIL import Image as PillowImage
+from PIL import ExifTags
 
 file = open('./Data.txt')
 content = file.readlines()
@@ -16,18 +19,37 @@ endpoint = content[8].replace("\n", "")
 
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 
-images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
+imageList = []
+file = open('./Data.txt')
+content = file.readlines()
+imageFolder = os.path.join (os.path.dirname(os.path.abspath(__file__)), content[9].replace("\n", ""))
+exportFolder = content[10].replace("\n", "")
 
-print("===== Describe an Image - local =====")
-local_image_path = os.path.join (images_folder, "26 (1).jpg")
-local_image = open(local_image_path, "rb")
+for file in os.listdir(imageFolder): 
+    imageList.append(file)
 
-description_result = computervision_client.describe_image_in_stream(local_image, 10)
+for img in imageList:
+    local_image = open(f"{imageFolder}\{img}", "rb")
+    description_result = computervision_client.describe_image_in_stream(local_image, 10)
 
-print("Description of local image: ")
-if (len(description_result.captions) == 0):
-    print("No description detected.")
-else:
-    for caption in description_result.captions:
-        print(caption.text.capitalize())
-print(description_result.captions)
+    if (len(description_result.captions) == 0):
+        print(f"No description detected for {img}.")
+    else:
+        imageDescription = description_result.captions[0].text.capitalize() + " - Generative AI"
+    
+    image = PillowImage.open(f"{imageFolder}\{img}")
+    XPTitle = 0x9C9B
+    exifdata = image.getexif()
+    
+    exifdata[XPTitle] = imageDescription.encode("utf16")
+    image.save(exportFolder + "\\" + img, exif=exifdata)
+    
+    
+    
+    
+
+
+
+
+
+
